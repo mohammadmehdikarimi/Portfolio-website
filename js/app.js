cc69b5cc0c700c32cc6734ffe23796b754ec0543
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2021-12-09 03:05:05 -0800

    function for hamberger

--- js/app.js
+++ js/app.js
@@ -6,4 +6,20 @@ $(document).ready(function() {
         appendDots:'.slider-dots',
         dotsClass:'dots'
     });
+
+// navbar hamberger(mobile)     
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click', function() {
+
+mobileNav.classList.add('open');
+
+});
+
+times.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+});
+
 });
